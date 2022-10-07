1b5fd5e17c88fefd923de48a0f4b2777bf31f030
mazy06 <miamazy@MacBook-Pro-de-Mia.local>
2022-10-07 22:01:27 +0200

    initial commit

---
+++ src/main/java/com/mazy/parkingmap/ParkingmapApplication.java
@@ -0,0 +1,13 @@
+package com.mazy.parkingmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazy/parkingmap/ParkingmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazy.parkingmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
